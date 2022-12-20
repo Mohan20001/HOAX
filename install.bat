7974be77178c64c8d5705cf1fe00363ef3f66acf
MohanPutta <61808180+Mohan20001@users.noreply.github.com>
2022-12-20 16:11:42 +0530

    bug fix

--- install.bat
+++ install.bat
@@ -1,11 +1,11 @@
 @echo off
 pip install pyngrok
 pip install pyfiglet
-pip install coloroma
+pip install colorama
 @REM ngrok config add-authtoken 299FED0QV81gQREsrXHLmdey2S2_5saXiXSDzhSgxp6AcWPgf //
 set /p a=authtoken : 
 ngrok config add-authtoken %a%
 echo %a% > ngrok_api_log.txt
 @REM 299FED0QV81gQREsrXHLmdey2S2_5saXiXSDzhSgxp6AcWPgf //
 pause
-python hoax.py
+python run.py
